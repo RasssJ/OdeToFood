bb9b7fae64b0c1333bdafb68e51d096005d406c4
Rasmus J <rasmus.jalakas11@gmail.com>
2021-11-15 15:59:57 +0200

    LogAttribute

---
+++ OdeToFood/Filter/LogAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Filter
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            base.OnResultExecuted(context);
+        }
+
+    }
+}
