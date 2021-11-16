1da19af1511cd022487f4efc6eb632cea7e6b984
Rasmus Jalakas <rasmus.jalakas11@gmail.com>
2021-11-16 09:46:29 +0200

    Add ReviewsController / RestaurantReview Model

---
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        // GET: ReviewsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ReviewsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReviewsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReviewsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReviewsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReviewsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReviewsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReviewsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ OdeToFood/Models/RestaurantReview.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Models
+{
+    public class RestaurantReview
+    {
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public string city { get; set; }
+
+        public string country { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
