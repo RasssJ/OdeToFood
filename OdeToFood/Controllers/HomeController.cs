9ca1dc325b9726db76be3413c83daf483629d4e6
Rasmus Jalakas <rasmus.jalakas11@gmail.com>
2021-11-30 09:16:14 +0200

    Add filtering to home page

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OdeToFood.Data;
 using OdeToFood.Models;
 using System;
 using System.Collections.Generic;
@@ -11,40 +12,47 @@ namespace OdeToFood.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
-		public HomeController(ILogger<HomeController> logger)
+		private ApplicationDbContext _context;
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
 		{
 			_logger = logger;
+			_context = context;
 		}
-
-
-
-		public IActionResult Index()
+		public IActionResult Index(string searchTerm = null)
 		{
-			var controller = RouteData.Values["controller"];
-			var action = RouteData.Values["action"];
-			var id = RouteData.Values["id"];
+			var model = _context.Restaurants
+				.OrderByDescending(
+					r => r.Reviews.Average(review => review.Rating)
+					)
+				.Where(r => searchTerm == null || r.Name.Contains(searchTerm))
+				.Select(r => new RestaurantListViewModel
+				{
+					Id = r.Id,
+					Name = r.Name,
+					City = r.City,
+					Country = r.Country,
+					CountOfReviews = r.Reviews.Count
+				});
 
-			ViewBag.Message = $"{controller}::{action} {id}";
-
-			return View();
-		}
-		public IActionResult About()
-		{
-			var model = new AboutModel()
-			{
-				Name = "Rasmus Jalakas",
-				Location = "Tallinn"
-			};
 			return View(model);
-		}
-		public IActionResult Privacy()
+	}
+	public IActionResult About()
+	{
+		var model = new AboutModel()
 		{
-			return View();
-		}
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
+			Name = "Kristjan Kivikangur",
+			Location = "Tallinn"
+		};
+		return View(model);
+	}
+	public IActionResult Privacy()
+	{
+		return View();
+	}
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 	}
 }
