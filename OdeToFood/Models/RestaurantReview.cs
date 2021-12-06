e5e95b54e6129743da71a1e6cd5707f6e1cab119
Rasmus Jalakas <rasmus.jalakas11@gmail.com>
2021-12-06 15:43:38 +0200

    Add validation scripts section and annotate review for validation and display

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,13 @@ namespace OdeToFood.Models
 	public class RestaurantReview
 	{
 		public int Id { get; set; }
+		[Range(1, 10)]
 		public int Rating { get; set; }
+		[Required]
+		[StringLength(1024)]
 		public string Body { get; set; }
+		[Display(Name = "User Name")]
+		[DisplayFormat(NullDisplayText = "anonymous")]
 		public string ReviewerName { get; set; }
 		public int RestaurantId { get; set; }
 	}
--- OdeToFood/Models/ViewModels/ReviewViewModel.cs
+++ OdeToFood/Models/ViewModels/ReviewViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace OdeToFood.Models.ViewModels
 	public class ReviewViewModel
 	{
 		public int Id { get; set; }
+		[Range(1, 10)]
 		public int Rating { get; set; }
+		[Required]
+		[StringLength(1024)]
 		public string Body { get; set; }
 	}
 }
