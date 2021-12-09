0e54473fa39b8443edf2f82cb742c29d3ab307a9
Rasmus Jalakas <rasmus.jalakas11@gmail.com>
2021-12-09 16:10:27 +0200

    Add Async search and autocomplete

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -12,74 +12,73 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private ApplicationDbContext _context;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
+		private ApplicationDbContext _context;
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+		{
+			_logger = logger;
+			_context = context;
+		}
+
+		public IActionResult Autocomplete(string term)
+		{
+			var model = _context.Restaurants
+				.Where(r => r.Name.StartsWith(term))
+				.Take(10)
+				.Select(r => new
+				{
+					label = r.Name
+				});
+			return Json(model);
+		}
+
+		public IActionResult Index(string searchTerm = null)
+		{
+
+			var model = _context.Restaurants
+				.OrderByDescending(r => r.Reviews.Average(review => review.Rating)
+				)
+				.Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
+				.Select(r => new RestaurantListViewModel
+				{
+					Id = r.Id,
+					Name = r.Name,
+					Country = r.Country,
+					City = r.City,
+					CountOfReviews = r.Reviews.Count
+
+				});
+
+			if (Request.IsAjaxRequest())
+			{
+				return PartialView("_Restaurants", model);
+			}
+
+			return View(model);
+		}
+		public IActionResult About()
+		{
+			var model = new AboutModel()
+			{
+				Name = "Rasmus Jalakas",
+				Location = "Tallinn"
+			};
+			return View(model);
+		}
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
 
 
-        public IActionResult Index(string searchTerm = null)
-        {
-            //var model =
-            //    from r in _context.Restaurants
-            //    orderby r.Reviews.Average(review => review.Rating) descending
-            //    select new RestaurantListViewModel
-            //    {
-            //        Id = r.Id,
-            //        Name = r.Name,
-            //        City=r.City,
-            //        Country= r.Country,
-            //        CountOfReviews = r.Reviews.Count()
-            //    };
-            var model = _context.Restaurants
-                .OrderByDescending(
-                r => r.Reviews.Average(review => review.Rating)
-                )
-                .Where(r => searchTerm == null || r.Name.Contains(searchTerm))
-                .Take(10)
-                .Select(r => new RestaurantListViewModel
-                {
-                    Id = r.Id,
-                    Name = r.Name,
-                    City = r.City,
-                    Country = r.Country,
-                    CountOfReviews = r.Reviews.Count()
-                });
-
-            if (Request.IsAjaxRequest())
-            {
-                return PartialView("_Restaurants", model);
-            }
-
-            return View(model);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        public IActionResult About()
-        {
-            var model = new AboutModel()
-            {
-                Name = "Marko Eduard Tagoma",
-                Location = "Tallinn"
-            };
-
-            return View(model);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-    }
+	}
 }
--- OdeToFood/wwwroot/js/site.js
+++ OdeToFood/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(function () {
+
+    let ajaxFormSubmit = function () {
+        const $form = $(this);
+        const options = {
+            url: $form.attr("action"),
+            type: $form.attr("method"),
+            data: $form.serialize()
+        }
+        $.ajax(options.done(function (data) {
+            const $target = $($form.attr("data-otf-target"));
+            $target.replaceWith(data);
+        }));
+        return false;
+
+    };
+    const createAutocomplete = function () {
+        const $input = $(this);
+        const options = {
+            source: $input.attr("data-otf-autocomplete")
+        };
+        $input.autocomplete(options);
+    };
+
+    $("form [data-otf-ajax='true']").submit(ajaxFormSubmit);
+    $("input[data-otf-autocomplete]").each(createAutocomplete);
+
+
+});
